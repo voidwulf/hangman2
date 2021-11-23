e967e71f18da5b95b9e3aaf1620bf6fe94a429e8
Martin Starčević <martin.starcevic989@gmail.com>
2021-11-23 19:57:02 +0000

    setup words, and visuals

---
+++ hangman_visual.py
@@ -0,0 +1,111 @@
+stage = [#final failed attempt - whole body with eyes in XX 
+             #signaling the end game
+         """
+            ______________
+            |  /         | 
+            | /        (X X)  (Bleh...)
+            |/          |o|  /
+            |            |         
+            |           /|\          
+            |          / | \.
+            |           / \.
+            |          /   \.
+            |
+        """
+        ,
+        #sixth failed attempt - body is complete, one last try
+        """
+            ______________
+            |  /         | 
+            | /        (. .)  (It is getting intense)
+            |/          |-|  /
+            |            |         
+            |           /|\          
+            |          / | \.
+            |           / \.
+            |          /   \.
+            |
+        """
+        ,
+        #fifth failed attempt - only one leg is missing 
+        """
+            ______________
+            |  /         | 
+            | /        (. .)  (I'ma one legged pirate!)
+            |/          |-|  /
+            |            |         
+            |           /|\          
+            |          / | \.
+            |           / 
+            |          /   
+            |
+        """
+        ,
+        #fourth failed attempt - upper body compleate
+        """
+            ______________
+            |  /         | 
+            | /        (. .)  (I'am geodude, no wait..)
+            |/          |-|  /
+            |            |         
+            |           /|\          
+            |          / | \.
+            |            
+            |            
+            |
+        """
+        ,
+        #third failed attempt - head, torso and right arm that look like lambda
+        """
+            ______________
+            |  /         | 
+            | /        (. .)  (Lambda, lambda, lambda)
+            |/          |-|  /
+            |            |         
+            |           /|          
+            |          / | 
+            |            
+            |            
+            |
+        """
+        ,
+        #second failed attempt - head and torso that look like lollipop
+        """
+            ______________
+            |  /         |
+            | /        (. .)  (Lollipop lollipop lolli lolli lollipop!)
+            |/          |-|  /
+            |            |   
+            |            | 
+            |            |
+            |            
+            |            
+            |
+        """
+        ,
+        #first failed attempt - a head appears
+                """
+            ______________
+            |  /         | 
+            | /        (. .)  (Huh... what am I doing here?)
+            |/          |-|  /
+            |        
+            |        
+            |
+            |
+            |
+            |
+        """
+        ,
+        #starting empty gallow
+        """
+            ___________
+            | /        | 
+            |/        
+            |          
+            |          
+            |
+            |
+            |
+        """
+         ]
--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random #import random module from library
+from words import word_list #import list of words from words.py
+from hangman_visual import stage # import hangman visual from hangman_visual.py
+
+#inspired by https://github.com/kiteco/python-youtube-code/tree/master/build-hangman-in-python
+def get_word():
+    """
+    function that retrives a random worl from list of words
+    """
+    word = random.choice(word_list)
+    return word.upper()
+
